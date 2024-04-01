c9420f2e8674014a31b846f8fbe72acc00e5c933
AmgaadShokrallah <amgaad@example.com>
2024-04-01 16:20:21 +0000

    commit

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,6 @@
 
 from flask import jsonify
 from api.v1.views import app_views
-from models import storage
 
 
 @app_views.route('/status')
@@ -11,17 +10,3 @@ def api_status():
     '''JSON Responses'''
     response = {'status': "OK"}
     return jsonify(response)
-    
-@app_views.route('/status')
-def get_stats():
-     '''JSON Responses'''
-     stats = {
-     'amenities': storage.count('Amenity'),
-     'cities': storage.count('City'),
-     'places': storage.count('Place'),
-     'reviews': storage.count('Review'),
-     'states': storage.count('State'),
-     'users': storage.count('User),
-     }
-     
-     return jsonify(stats)
