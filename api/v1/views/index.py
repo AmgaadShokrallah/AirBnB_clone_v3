45f06930333b5501fbf3ae7b57daf547ba6855fc
AmgaadShokrallah <amgaad@example.com>
2024-04-01 16:09:52 +0000

    commit

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,24 @@
-#!/usr/bin/python3
+#!/usr/bin/python3xx
 '''api status'''
-
+import models
+from models import storage
+from models.base_model import BaseModel
 from flask import jsonify
 from api.v1.views import app_views
 
 
-@app_views.route('/status')
-def api_status():
+@app_views.route('/status', strict_slashes=False)
+def returnstuff():
+    '''return stuff'''
+    return jsonify(status='OK')
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stuff():
     '''JSON Responses'''
-    response = {'status': "OK"}
-    return jsonify(response)
+    stats = {'states': State, 'users': User,
+            'amenities': Amenity, 'cities': City,
+            'places': Place, 'reviews': Review}
+    for key in stats:
+        stats[key] = storage.count(stats[key])
+    return jsonify(stats)
