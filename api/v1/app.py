77b5238ac85d3dbe3651bbe6224948e06138f290
AmgaadShokrallah <amgaad@example.com>
2024-04-01 16:40:12 +0000

    commit

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,8 +13,24 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
+@app.teardown_appcontext
+def teardown_engine(exception):
+    '''
+
+    '''
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    '''
+
+    '''
+    response = {"error": "Not found")
+    return jsonify(response), 404
+
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = getenv('HBNB_API_PORT', 5000)
-    app.run(host=host, port=port, threaded=True)
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(getenv('HBNB_API_PORT', 5000)
+    app.run(debug=True, host=HOST, port=PORT, threaded=True)
