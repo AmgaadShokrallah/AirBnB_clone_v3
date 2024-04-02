6432c49c945ec6d74c4f91ec3a9a428bff291e25
AmgaadShokrallah <138812734+AmgaadShokrallah@users.noreply.github.com>
2024-04-02 18:59:57 +0000

    commit

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -38,12 +38,12 @@ def update_city(city_id):
     updates specific City object by ID
     """
     if request.content_type != 'application/json':
-         return abort(400, 'Not a JSON')
+        return abort(400, 'Not a JSON')
     city = storage.get("City", city_id)
     if city:
         if not request.get_json():
             return abort(400, 'Not a JSON')
-        data = request.get_json():
+        data = request.get_json()
         ignore_keys = ['id', 'state_id', 'created_at', 'updated_at']
         for key, value in data.items():
             if key not in ignore_keys:
@@ -51,7 +51,8 @@ def update_city(city_id):
         city.save()
         return jsonify(city.to_dict()), 200
     else:
-        return abort(404) 
+        return abort(404)
+
 
 @app_views.route("/cities/<city_id>",  methods=["DELETE"],
                  strict_slashes=False)
@@ -81,8 +82,7 @@ def city_create(state_id):
     if not state:
         return abort(404)
     if not request.get_json():
-    	return abort(400, 'Not a JSON')
-
+        return abort(400, 'Not a JSON')
     data = request.get_json()
     if 'name' not in data:
         return abort(400, 'Missing name')
